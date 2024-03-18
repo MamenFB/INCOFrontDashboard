7d1791483a3486a0fb2a7062ee86a1e27092f5d7
RameshNada <ramesh.nadarajan@gmail.com>
2024-03-18 10:10:13 +0100

    iconchage

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -9,7 +9,6 @@ import "./style.css";
 import CalendarIcon from "./CalendarIcon"; // Update the path accordingly
 import Navbar from "./Navbar";
 
-
 const Dashboard = () => {
   const navigate = useNavigate();
   axios.defaults.withCredentials = true;
@@ -52,7 +51,7 @@ const Dashboard = () => {
                   to="/dashboard/student"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-people ms-2"></i>
+                  <i className="fs-4 bi-mortarboard ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Manage Students
                   </span>
@@ -63,7 +62,7 @@ const Dashboard = () => {
                   to="/dashboard/employee"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-people ms-2"></i>
+                  <i className="fs-4 bi-person-badge ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">
                     Manage Teachers
                   </span>
@@ -74,7 +73,7 @@ const Dashboard = () => {
                   to="/dashboard/course"
                   className="nav-link px-0 align-middle text-white"
                 >
-                  <i className="fs-4 bi-columns ms-2"></i>
+                  <i className="fs-4 bi-book-half ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Course</span>
                 </Link>
               </li>
@@ -109,10 +108,12 @@ const Dashboard = () => {
           </div>
         </div>
         <div className="col p-0 m-0">
-          <div className="p-2 d-flex justify-content-end" style={{ backgroundColor: "navy" }}>
+          <div
+            className="p-2 d-flex justify-content-end"
+            style={{ backgroundColor: "navy" }}
+          >
             {/* <h4>INCO Dashboard</h4> */}
             <Navbar />
-         
           </div>
           <Outlet />
         </div>
