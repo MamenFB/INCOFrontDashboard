63f1f275d754465c74979258c897d00c0933f29a
MiguelMeixoeiro <m.meixoeiro@gmail.com>
2024-03-27 12:38:35 +0100

    changes

--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+// Profile.jsx
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.get('/api/employee', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        setUserData(response.data);
+        console.log(response);
+      })
+      .catch(error => {
+        console.error('Error fetching user profile data:', error);
+      });
+    } else {
+      // El usuario no ha iniciado sesión, manejar este caso según tus necesidades
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Perfil de Usuario</h1>
+      {userData ? (
+        <div>
+          <p>ID: {userData.userid}</p>
+          <p>Username: {userData.username}</p>
+          <p>Email: {userData.email}</p>
+        </div>
+      ) : (
+        <p>Cargando...</p>
+      )}
+    </div>
+  );
 };
 
 export default Profile;
